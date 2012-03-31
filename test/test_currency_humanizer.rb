4e5e01cca67f41404f7eebce7663bd74fea35335
Steven Boles <boles.steven@gmail.com>
2012-03-31 16:19:44 -0400

    Refactored: updated tests. not sure if this is any better

--- test/test_currency_humanizer.rb
+++ test/test_currency_humanizer.rb
@@ -3,29 +3,15 @@ require 'helper'
 class TestCurrencyHumanizer < Test::Unit::TestCase
 
   def test_invalid_currencies
-    assert !CurrencyHumanizer.valid_currency_string?("")
-    assert !CurrencyHumanizer.valid_currency_string?("a")
-    assert !CurrencyHumanizer.valid_currency_string?(".")
-    assert !CurrencyHumanizer.valid_currency_string?(".0")
-    assert !CurrencyHumanizer.valid_currency_string?(".1")
-    assert !CurrencyHumanizer.valid_currency_string?("1.")
-    assert !CurrencyHumanizer.valid_currency_string?("1.0")
-    assert !CurrencyHumanizer.valid_currency_string?("0.0")
-    assert !CurrencyHumanizer.valid_currency_string?("0.1")
-    assert !CurrencyHumanizer.valid_currency_string?("$0.01")
-    assert !CurrencyHumanizer.valid_currency_string?("$0")
-    assert !CurrencyHumanizer.valid_currency_string?("1,000.00")
+    ["", "a", ".", ".0", ".1", "1.", "1.0", "0.0", "0.1", "$0.01", "$0", "1,000.00"].each do |invalid_string|
+      assert !CurrencyHumanizer.valid_currency_string?(invalid_string), "#{invalid_string} should be invalid"
+    end
   end
 
   def test_valid_currencies
-    assert CurrencyHumanizer.valid_currency_string?("0.00")
-    assert CurrencyHumanizer.valid_currency_string?("1.00")
-    assert CurrencyHumanizer.valid_currency_string?("1.01")
-    assert CurrencyHumanizer.valid_currency_string?("123.99")
-    assert CurrencyHumanizer.valid_currency_string?("1234.99")
-    assert CurrencyHumanizer.valid_currency_string?("1234567.99")
-    assert CurrencyHumanizer.valid_currency_string?("00000000.99")
-    assert CurrencyHumanizer.valid_currency_string?("01928740981273049812093847012981029870.01")
+    ["0.00", "1.00", "1.01", "123.99", "1234.99", "1234567.99", "00000000.99", "01928740981273049812093847012981029870.01"].each do |valid_string|
+      assert CurrencyHumanizer.valid_currency_string?(valid_string), "#{valid_string} should be valid"
+    end
   end
 
   def test_humanize_with_invalid_currencies
@@ -45,12 +31,12 @@ class TestCurrencyHumanizer < Test::Unit::TestCase
   end
 
   def test_last_3_digits
-    assert_equal "000", CurrencyHumanizer.last_3_digits("0")
-    assert_equal "000", CurrencyHumanizer.last_3_digits("00")
-    assert_equal "000", CurrencyHumanizer.last_3_digits("000")
-    assert_equal "001", CurrencyHumanizer.last_3_digits("1")
-    assert_equal "001", CurrencyHumanizer.last_3_digits("01")
-    assert_equal "001", CurrencyHumanizer.last_3_digits("001")
+    ["0", "00", "000", "0000"].each do |currency_string|
+      assert_equal "000", CurrencyHumanizer.last_3_digits(currency_string), "last 3 digits of #{currency_string} should be 000"
+    end
+    ["1", "01", "001", "0001"].each do |currency_string|
+      assert_equal "001", CurrencyHumanizer.last_3_digits(currency_string), "last 3 digits of #{currency_string} should be 001"
+    end
   end
 
 end
