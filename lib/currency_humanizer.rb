d86424d7b348f22e2615fb6683d89c00d90cd999
Steven Boles <boles.steven@gmail.com>
2012-03-31 23:01:28 -0400

    fixed bug in hundredths_part and added tenths_part method

--- lib/currency_humanizer.rb
+++ lib/currency_humanizer.rb
@@ -35,8 +35,38 @@ class CurrencyHumanizer
     if "0".eql?(digit)
       return ""
     else
-      return "#{digit} hundred"
+      return "#{ONES[digit]} hundred"
     end
   end
+
+  # Returns human readable English representation of tenths digit
+  def self.tenths_part(digit)
+    if "0".eql?(digit)
+      return ""
+    else
+      return TENS[digit]
+    end
+  end
+
+  ONES = {"0" => "zero",
+          "1" => "one",
+          "2" => "two",
+          "3" => "three",
+          "4" => "four",
+          "5" => "five",
+          "6" => "six",
+          "7" => "seven",
+          "8" => "eight",
+          "9" => "nine"}
+
+  TENS = {"1" => "ten",
+          "2" => "twenty",
+          "3" => "thirty",
+          "4" => "fourty",
+          "5" => "fifty",
+          "6" => "sixty",
+          "7" => "seventy",
+          "8" => "eighty",
+          "9" => "ninety"}
 
 end
--- test/test_currency_humanizer.rb
+++ test/test_currency_humanizer.rb
@@ -40,10 +40,29 @@ class TestCurrencyHumanizer < Test::Unit::TestCase
   end
 
   def test_hundredths_part
-    assert_equal "", CurrencyHumanizer.hundredths_part("0")
-    1.upto(9) do |n|
-      assert_equal "#{n} hundred", CurrencyHumanizer.hundredths_part(n.to_s), "#{n} should return '#{n} hundred'"
-    end
+    assert_equal "", CurrencyHumanizer.hundredths_part("0"), "0 should return ''"
+    assert_equal "one hundred", CurrencyHumanizer.hundredths_part("1"), "1 should return 'one hundred'"
+    assert_equal "two hundred", CurrencyHumanizer.hundredths_part("2"), "2 should return 'two hundred'"
+    assert_equal "three hundred", CurrencyHumanizer.hundredths_part("3"), "3 should return 'three hundred'"
+    assert_equal "four hundred", CurrencyHumanizer.hundredths_part("4"), "4 should return 'four hundred'"
+    assert_equal "five hundred", CurrencyHumanizer.hundredths_part("5"), "5 should return 'five hundred'"
+    assert_equal "six hundred", CurrencyHumanizer.hundredths_part("6"), "6 should return 'six hundred'"
+    assert_equal "seven hundred", CurrencyHumanizer.hundredths_part("7"), "7 should return 'seven hundred'"
+    assert_equal "eight hundred", CurrencyHumanizer.hundredths_part("8"), "8 should return 'eight hundred'"
+    assert_equal "nine hundred", CurrencyHumanizer.hundredths_part("9"), "9 should return 'nine hundred'"
+  end
+
+  def test_tenths_part
+    assert_equal "",        CurrencyHumanizer.tenths_part("0")
+    assert_equal "ten",     CurrencyHumanizer.tenths_part("1"), "1 should return 'ten'"
+    assert_equal "twenty",  CurrencyHumanizer.tenths_part("2"), "2 should return 'twenty'"
+    assert_equal "thirty",  CurrencyHumanizer.tenths_part("3"), "3 should return 'thirty'"
+    assert_equal "fourty",  CurrencyHumanizer.tenths_part("4"), "4 should return 'fourty'"
+    assert_equal "fifty",   CurrencyHumanizer.tenths_part("5"), "5 should return 'fifty'"
+    assert_equal "sixty",   CurrencyHumanizer.tenths_part("6"), "6 should return 'sixty'"
+    assert_equal "seventy", CurrencyHumanizer.tenths_part("7"), "7 should return 'seventy'"
+    assert_equal "eighty",  CurrencyHumanizer.tenths_part("8"), "8 should return 'eighty'"
+    assert_equal "ninety",  CurrencyHumanizer.tenths_part("9"), "9 should return 'ninety'"
   end
 
 end
